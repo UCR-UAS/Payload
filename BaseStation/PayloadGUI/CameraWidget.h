d3486cd826f2a4d867cb41f38ebb790f4070c208
Stanley Okwuoma <sokwu001@ucr.edu>
2016-04-15 14:44:56 -0700

    sup fam

--- BaseStation/PayloadGUI/CameraWidget.h
+++ BaseStation/PayloadGUI/CameraWidget.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "StarPacket0x01.h"
 #include <iostream>
 #include <string>
 #include <QPushButton>
@@ -7,33 +8,28 @@
 #include <QLineEdit>
 #include <QString>
 #include <QLabel>
+#include <QObject>
 
 //! Camera Widget Class
 class CameraWidget : public QDockWidget{
+	Q_OBJECT
 	public:
 		explicit CameraWidget(QWidget *parent=0);
 	private:
 		QPushButton *cameraButton;
 		QLineEdit   *cameraStatus;
 		QLabel	    *cameraLabel;
+		QPushButton *statusButton;
 		QLineEdit   *imageCounter;
 		QLabel      *counterLabel;
+
+		StarPacket0x01 testPacket; //for testing purposes.
 
 	signals:
 
 	public slots:
-		//Start camera capture function
-		/*
-		void startCamera() {
-			if( ) {
-			
-			}
-		}
-		//Stop camera capture function
-		void stopCamera() {
-			if( ) {
-			
-			}	
-		}
-		*/
+		//Start or stop camera capture function
+		void setCameraCapture();
+		void checkStatus(); //remove if not necessary
+		
 };
